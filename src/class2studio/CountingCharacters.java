5eadb4d0d2afc6837dad326a0e5100735d0f3fbf
Matt McKeever <mmckeever23@gmail.com>
2022-08-25 20:20:57 -0500

    Completed Studio

--- src/class2studio/CountingCharacters.java
+++ src/class2studio/CountingCharacters.java
@@ -1,20 +1,24 @@
 package class2studio;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CountingCharacters {
     public static void main(String[] args) {
         String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         char[] charactersInString = myString.toCharArray();
         HashMap<Character, Integer> result = new HashMap<>();
+
         for (char character : charactersInString) {
-            if (result.containsKey(character) == true) {
+            if (result.containsKey(character)) {
                 result.put(character, result.get(character) + 1);
             } else {
                 result.put(character, 1);
             }
             ;
         }
-        System.out.println(result);
+        for (Map.Entry<Character, Integer> oneChar : result.entrySet()) {
+        System.out.println(oneChar.getKey() + ": " + oneChar.getValue());
+        }
     }
 };
